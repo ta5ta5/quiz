faff6e2d1f7fe44d9313826cb050388259491217
Bassem NAFFETI <naffeti.bassem@gmail.com>
2015-11-03 14:22:04 +0100

    refactoring Parser.java

--- Parser.java
+++ Parser.java
@@ -1,42 +1,65 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+
+	/** The file attribute */
+	private File file;
+
+	/**
+	 * getContent method.
+	 * @return content
+	 */
+	public String getContent(boolean WithoutUnicode) {
+		String output = "";
+		try {
+			FileInputStream i = new FileInputStream(file);
+			int data;
+			while ((data = i.read()) > 0) {
+				if (WithoutUnicode && (data < 0x80)) {
+					output += (char) data;
+				} else {
+					output += (char) data;
+				}
+			}
+		} catch (FileNotFoundException fnfException) {
+			System.err.println("File not fount : " + fnfException.getMessage());
+		} catch (IOException ioException) {
+			System.err.println("caught exception: " + ioException.getMessage());
+		}
+		return output;
+	}
+
+	/**
+	 * saveContent method.
+	 * @throws IOException IOException
+	 */
+	public void saveContent(String content) throws IOException {
+		FileOutputStream o = new FileOutputStream(file);
+		for (int i = 0; i < content.length(); i += 1) {
+			o.write(content.charAt(i));
+		}
+	}
+	
+	/**
+	 * Setter of file attribute.
+	 * @param f File
+	 */
+	public synchronized void setFile(File f) {
+		this.file = f;
+	}
+
+	/**
+	 * Getter of file attribute.
+	 * @return file
+	 */
+	public synchronized File getFile() {
+		return this.file;
+	}
 }
